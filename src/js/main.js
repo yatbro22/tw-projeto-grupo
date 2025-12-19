13f83f5455ed9a1feb034c48bd37e41cb5b98212
Miguel Silva <miguelklogsilva@gmail.com>
2025-12-19 23:40:00 +0000

    testing out filters

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,70 @@
 import "../scss/styles.scss";
 import movies from "../db/imdb_top_1000.json";
 
-// Import all of Bootstrap's JS
-import * as bootstrap from "bootstrap";
+const tableBody = document.querySelector("#body");
+const inputSearch = document.querySelector("#search");
+const select = document.querySelector("#select");
 
-// movieData is already a JS object, no JSON.parse
+let search = "";
+let genre = "";
 
-const movieData = movies.MovieData;
+const distinctGenres = new Set(
+  movies.MovieData.flatMap((movie) =>
+    movie.Genre.split(",").map((genre) => genre.trim())
+  )
+);
+
+distinctGenres.forEach(addGenreOption);
+
+tableBody.innerHTML = movies.MovieData.map(
+  (movie) =>
+    `<tr>
+        <td>${movie.Series_Title}</td>
+        <td>${movie.Genre}</td>
+    </tr>
+    `
+).join(" ");
+
+inputSearch.addEventListener("input", (e) => {
+  search = e.target.value;
+  updateTableData();
+});
+
+select.addEventListener("input", (e) => {
+  genre = e.target.value;
+  updateTableData();
+});
+
+function updateTableData() {
+  const data = movies.MovieData.filter((movie) => {
+    const title = `${movie.Series_Title}`;
+    const localGenre = `${movie.Genre}`;
+    return search.length
+      ? title.length
+        ? title.includes(search) && localGenre.includes(genre)
+        : title.includes(search)
+      : title.length
+      ? title.includes(search)
+      : true;
+  });
+
+  tableBody.innerHTML = data
+    .map(
+      (movie) =>
+        `<tr>
+        <td>${movie.Series_Title}</td>
+        <td>${movie.Genre}</td>
+    </tr>
+    `
+    )
+    .join(" ");
+}
+
+function addGenreOption(genre) {
+  var option = document.createElement("option");
+
+  option.text = genre;
+  option.value = genre;
+
+  select.add(option, null);
+}
