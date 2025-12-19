40405c67df698fea316a2c541b34b043f9868c78
Miguel Silva <miguelklogsilva@gmail.com>
2025-12-19 22:46:32 +0000

    change json import

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
-const path = require('path')
+const path = require("path");
 
 export default {
-    root: path.resolve(__dirname, 'src'),
-    resolve: {
-        alias: {
-            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-        }
+  root: path.resolve(__dirname, "src"),
+  build: {
+    outDir: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
     },
-    server: {
-        port: 8080,
-        hot: true
-    },
-    assetsInclude: ['**/*.json']
-}
+  },
+  server: {
+    port: 8080,
+    hot: true,
+  },
+};
