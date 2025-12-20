72c1465afc0aeabb4cd62eec697e188e79171587
Miguel Silva <115943663+mrjshzk@users.noreply.github.com>
2025-12-20 16:24:25 +0000

    remove unnecessary comments and slight refactor to card container code functions

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,15 @@
-// Import our custom CSS
 import "../scss/styles.scss";
 import movies from "../db/imdb_top_1000.json";
 import grinch from "../img/grinch.png";
 
-// const tableBody = document.querySelector("#body");
 const cardContainer = document.querySelector("#cardContainer");
 const inputSearch = document.querySelector("#search");
 const select = document.querySelector("#select");
-
-// reduce moviedata to only 100 items
-// movies.MovieData = movies.MovieData.slice(0, 20);
-console.log(movies.MovieData);
 
 // filter variables
 let search = "";
 let genre = "";
 
-// TODO something else than Set
 const distinctGenres = [
   "Drama",
   "Crime",
@@ -42,11 +35,7 @@ const distinctGenres = [
 ];
 
 distinctGenres.forEach(addGenreOption);
-
-console.log(grinch); // This should log the correct path like /img/grinch.png
-cardContainer.innerHTML = movies.MovieData.map((movie) =>
-  getCardFromMovieData(grinch, movie.Series_Title, movie.Overview, movie.Genre)
-).join(" ");
+UpdateCardContainerInnerHTML(movies.MovieData);
 
 inputSearch.addEventListener("input", (e) => {
   search = e.target.value;
@@ -76,7 +65,19 @@ function updateTableData() {
       return movie.Genre.toString().toLowerCase().includes(genre.toLowerCase());
     });
   }
+  UpdateCardContainerInnerHTML(filteredData);
+}
 
+function addGenreOption(genre) {
+  var option = document.createElement("option");
+
+  option.text = genre;
+  option.value = genre;
+
+  select.add(option, null);
+}
+
+function UpdateCardContainerInnerHTML(data) {
   cardContainer.innerHTML = filteredData
     .map((movie) =>
       getCardFromMovieData(
@@ -89,15 +90,6 @@ function updateTableData() {
     .join(" ");
 }
 
-function addGenreOption(genre) {
-  var option = document.createElement("option");
-
-  option.text = genre;
-  option.value = genre;
-
-  select.add(option, null);
-}
-
 function getCardFromMovieData(image, title, overview, genre) {
   return `<div class="card col-sm-6 col-md-4 col-lg-3" style="width: 18rem;">
   <img src="${image}" class="card-img-top" alt="Movie poster">
@@ -106,6 +98,5 @@ function getCardFromMovieData(image, title, overview, genre) {
     <p class="card-text">${overview}</p>
     <a href="#" class="btn btn-primary">${genre}</a>
   </div>
-</div>
-    `;
+</div>`;
 }
